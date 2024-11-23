532ea20ab02a65455197a052017489c6a2e53ccf
LeilaAimetova15 <leilochkazai@gmail.com>
2024-11-23 21:01:35 +0300

    Задача "Записать и запомнить": Создайте функцию custom_write(file_name, strings), которая принимает аргументы file_name - название файла для записи, strings - список строк для записи. Функция должна: Записывать в файл file_name все строки из списка strings, каждая на новой строке. Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>), а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell() перед записью.

---
+++ module_7_2.py
@@ -0,0 +1,22 @@
+def custom_write(file_name: str, strings: list):
+    strings_positions = {}
+    number_of_line = 1
+    file = open(file_name, 'w', encoding='utf-8')
+    for i in strings:
+        byte_of_line = file.tell()
+        file.write(f'{i}\n')
+        strings_positions[(number_of_line, byte_of_line)] = i
+        number_of_line += 1
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('text.txt', info)
+for elem in result.items():
+    print(elem)
